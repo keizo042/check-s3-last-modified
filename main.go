60c4c6598f1542cf8eedeba6a9b40fb04bbdcda9
Koichi Nakanishi <keizo042dev@gmail.com>
2019-02-20 17:46:00 +0900

    Add

---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+const (
+	DEFAULT_REGION   = "ap-northeast-1"
+	DEFAYLT_INTERVAL = 60 * 60 * 24
+)
+
+type Config struct {
+	Region   string
+	Bucket   string
+	Interval int
+	ID       string
+	Secret   string
+	Token    string
+}
+
+func main() {
+	var c Config
+	c.Bucket = *flag.String("bucket", "", "S3 bucket name absolute path")
+	c.ID = *flag.String("id", "", "AWS Account ID")
+	c.Secret = *flag.String("secret", "", "AWS Secret Token")
+	c.Token = *flag.String("token", "", "AWS Token")
+	c.Interval = *flag.Int("interval", DEFAYLT_INTERVAL, "interval seconds until S3 last modified")
+	c.Region = *flag.String("region", DEFAULT_REGION, "AWS region")
+
+	if c.Bucket == "" {
+		flag.Usage()
+		os.Exit(1)
+		return
+	}
+	if c.ID == "" {
+		flag.Usage()
+		os.Exit(1)
+		return
+	}
+	if c.Token == "" {
+		flag.Usage()
+		os.Exit(1)
+		return
+	}
+	if c.Secret == "" {
+		flag.Usage()
+		os.Exit(1)
+		return
+	}
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String(c.Region),
+		Credentials: credentials.NewStaticCredentials(c.ID, c.Secret, c.Token),
+	})
+	if err != nil {
+		fmt.Printf("WARNING: error: %v\n", err)
+		os.Exit(1)
+	}
+	svc := s3.New(sess)
+	input := &s3.ListObjectsInput{
+		Bucket: &c.Bucket,
+	}
+
+	output, err := svc.ListObjects(input)
+	if err != nil {
+		fmt.Printf("WARNING: error: %v\n", err)
+		os.Exit(1)
+		return
+	}
+	interval := time.Duration(c.Interval) * time.Second
+	period := time.Now().Add(interval)
+	for _, obj := range output.Contents {
+		if obj.LastModified.After(period) {
+			fmt.Printf("OK: last modified at %s\n", obj.LastModified.String())
+			return
+		}
+	}
+	os.Exit(1)
+	fmt.Printf("WARNING: not found modified object until %s\n", period.String())
+	return
+
+}
